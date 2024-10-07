ce3f2b5f3e61e77ac216c9670ebca9698f2f2a3a
Carlos Infante <carlos.infcar@educa.jcyl.es>
2024-10-07 11:55:54 +0200

    first commit

---
+++ ConfirmarDNI.js
@@ -0,0 +1,12 @@
+let ndni=prompt("Introduzca su DNI");
+const letrasDNI = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+document.write("Vamos a comprobar la letra del DNI");
+let numero = ndni.slice(0 , 8);
+let letra = ndni.slice(8).toUpperCase();
+let resto = numero % 23;
+    if(letra===letrasDNI[resto]){
+        alert("La letra es correcta");
+    }else{
+        alert("La letra es incorrecta");
+    }
+
