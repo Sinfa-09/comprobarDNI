698a23e3dbc9046e3890f67fe35b05048a04094e
Carlos Infante <carlos.infcar@educa.jcyl.es>
2024-10-08 14:07:57 +0200

    cuarto comit

--- ConfirmarDNI.js
+++ ConfirmarDNI.js
@@ -1,9 +1,8 @@
 "use strict"
 let ndni=prompt("Introduzca su DNI");
 const letrasDNI = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
-document.write("Vamos a comprobar la letra del DNI");
-let numero = ndni.slice(0 , 8);
-let letra = ndni.slice(8).toUpperCase();
+let numero = ndni.slice(0 , 8); //Con slice cojo los 8 primeros digitos de el ndni (el octavo no se coje)
+let letra = ndni.slice(8).toUpperCase(); //Y aqui cojo del octavo hacia adelante
 let resto = numero % 23;
     if(letra===letrasDNI[resto]){
         alert("La letra es correcta");
